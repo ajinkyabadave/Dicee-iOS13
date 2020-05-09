77c5b31cc754d38b345ba5f0fc192cabd9bb9112
Ajinkya Badave <badaveajinkya@gmail.com>
2020-05-09 15:09:20 +0530

    Final app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // hold the control button to connect the elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      //  diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+       // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("roll button pressed")
+        // text inside double quotes meansn the text is printed in the console
+      
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in:1...5)]
+        
+        diceImageView2.image = diceArray.randomElement()
+        
+        // with these two methods we can have randomize the dice
+        // the range func ... includes the upper and lower bound
+        // ..< will not include the upper bound
+    }
+    
+    
 }
 
